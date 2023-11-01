d11769e69e4d07c2222b8c4bd37cfef17babbbaa
Simbaqueba <juan.simbaqueba@urosario.edu.co>
2023-11-01 12:50:20 -0500

    JavaScript
    Apuntes de java y wea basica, se usa sumador de base

---
+++ assets/js/introduction.js
@@ -0,0 +1,35 @@
+//name = $name = _name
+
+let nombre = 'Pepe';
+let arreglo = [1,4,68,32];
+
+console.log(`él se llama ${nombre}\naa`); //print("él se llama", nombre, "\naa")
+
+let a=5;
+let b= 10;
+console.log(`La suma es ${a+b}`);
+console.log()
+document.write(`hello <br><h1>world</h1>`)
+
+/*Operadores
+Comparación
+== Igual ("2" = 2, true)
+=== Identico ("2" = 2, false)
+!= Diferente
+!== Totalmente diferente
+<, >, <=, >=
+
+Logicos 
+&& = And
+| | = Or
+! = No
+
+*/
+
+
+
+let num1 = parseInt(prompt("Escribir el primer número"))
+console.log(`${num1}`);
+let num2 = parseInt(prompt("Escribir el primer número"))
+console.log(`${num2}`);
+document.write(`La suma es ${num1+num2}`);
